d5caca80fb90187f2f7f8a5be105d2d2f04d10af
Admin <User@Lions-Mac.local>
2013-03-26 17:24:16 +0200

    heloo

---
+++ browser/browser/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  browser
+//
+//  Created by Admin on 26/03/2013.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ browser/browser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  browser
+//
+//  Created by Admin on 26/03/2013.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
